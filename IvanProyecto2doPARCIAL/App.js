3f90559fea1974c8baa6489ced78ca08cc3015de
124051297 <124051297@upq.edu.mx>
2025-10-28 17:53:04 -0600

    Funcionamiento y eliminacion de codigo basura

--- IvanProyecto2doPARCIAL/App.js
+++ IvanProyecto2doPARCIAL/App.js
@@ -1,10 +1,7 @@
-//1. imports: Zona de importaciones 
 
-//Esta pantalla servirá para solo mostrar cada "screen" que hagamos hasta el segundo parcial 
-
-//import { StatusBar } from 'expo-status-bar';
-//import { StyleSheet, Text, View, Button} from 'react-native'; //Aquí agregamos los componentes que tenemos en este archivo
-import MenuScreen from './screens/MenuScreen'; 
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button} from 'react-native'; 
+import MenuScreen from './screensProyecto/menuScreen'; 
 
 
 //2. Main: Zona de componentes
