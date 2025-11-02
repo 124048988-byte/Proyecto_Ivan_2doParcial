34a463bae1e2ccb2072862e505d94a560cac7b95
marfer246 <124049489@upq.edu.mx>
2025-11-01 22:53:20 -0600

    dificultades para bajar el scroll

--- IvanProyecto2doPARCIAL/screensProyecto/PrincipalScreen.js
+++ IvanProyecto2doPARCIAL/screensProyecto/PrincipalScreen.js
@@ -1,24 +1,33 @@
 import { useState } from 'react'
-import { Text, StyleSheet, View, TouchableOpacity, ScrollView } from 'react-native'
+import { Text, StyleSheet, View, TouchableOpacity, ScrollView, Image } from 'react-native'
 
 export default function MioScreen () {
-  const [items, setItems] = useState (["op 1", "op 2", "op 3"])
+  const [items, setItems] = useState ([
+    {type: 'image', source: require ('../assets/Recursos/4.jpg')},
+    {type: 'image', source: require ('../assets/Recursos/4.jpg')},
+    {type: 'image', source: require ('../assets/Recursos/4.jpg')}
+  ])
 
   return (
     <View style={styles.container}>
     <ScrollView 
      style = {styles.Scroll }
-     contentContainerStyle = {style.scrollContainer}
+     contentContainerStyle = {styles.scrollContainer}
      horizontal = {true}
      showsHorizontalScrollIndicator= {true}
      persistentScrollbar= {true}
      >
-     {items.map((item, index)(
-     <View Key= {index} style= {styles.box}>
-       <Text style = {styles.boxText} >{item}</Text>
+     {items.map((item, index) => (
+     <View key= {index} style= {styles.box}>
+      {item.type === 'image' ? (
+       <Image source={item.source} style={styles.image} /> 
+      ): (
+        <Text style = {styles.boxText} >{item.content}</Text>
+      )}    
      </View>
-     ))}
-    <ScrollView/>
+     ))} 
+
+    </ScrollView>
 
       <View style ={styles.header}>  
       <Text style = {styles.texto} > Ahorra+App </Text>
@@ -36,7 +45,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'blue',
-    justifyContent: 'center',
+    justifyContent: 'left',
     alignItems: 'center'
   },
   texto: {
@@ -52,7 +61,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     textAlign: 'left',   
     //marginLeft: 0 
-    alignSelf:'left',
+    alignSelf:'flex-start',
     paddingVertical: 60, // alto
     paddingHorizontal: 0, // ancho
 
@@ -60,7 +69,7 @@ const styles = StyleSheet.create({
      texto3: {
     color: 'white',
     marginBottom: 20,
-    textAlign: 'left',   
+    textAlign: 'flex-start',   
     marginVertical: -80, 
     alignSelf:'left',
     paddingVertical: 0, // alto
@@ -69,17 +78,23 @@ const styles = StyleSheet.create({
   },
 
   boton: {
+  position :  'absolute',
+  bottom: 300,
+  alignSelf:  'center',
   backgroundColor: 'white',
   paddingVertical: 20, // alto
   paddingHorizontal: 60, // ancho
   borderRadius: 10, // bordes redondeados
   minWidth: 200, //asegura un ancho minimo
-  alignItems: 'center' ///centrar
+  alignItems: 'center', ///centrar
+  //marginTop: -30,
 
   },
   botontexto: {
    color: 'purple',
-   fontSize: 18 
+   fontSize: 18 ,
+   
+ 
   },
   header: {// todo este centrado
     position:  'absolute',
@@ -87,7 +102,39 @@ const styles = StyleSheet.create({
  //   alignItems:  'center',
     width: '100%', //para centrar tmb
     paddingHorizontal: 20
+  },
+  box: {
+    backgroundColor: 'white',
+    padding: 20,
+    marginHorizontal:10,
+    borderRadius:10
+  },
+  boxText:{
+    color: 'purple',
+    fontSize:  18, 
+    
+  },
+
+  scroll: {
+   // position: 'absolute',
+    top: 250,
+    height : 100,
+    width:  '100%',
+    paddingHorizontal:10,
+    backgroundColor:  'white',
+    zIndex:1
+    marginTop: 2500,
+  },
+  scrollContainer:{ //centra y limita el scroll
+    alignItems:'center',
+    justifyContent: 'center',
+  },
+  image: {
+width: 100,
+height: 100,
+borderRadius: 10,
+resizeMode: 'cover',
+
   }
-  
 
 })
